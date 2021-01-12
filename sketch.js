66a225a314425c39b8552952a6aea0d85cdbe3c6
Razon275 <73532970+Razon275@users.noreply.github.com>
2021-01-12 16:51:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var ground;
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+  ground=new Ground(700,240,240,20)
+ 
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  ground.display()
+  drawSprites();
+ 
+}
